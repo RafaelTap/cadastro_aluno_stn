a8d72d48cdfa9b1ea16b708923e5666bdb61977b
RafaelTaparica <rafael_taparica@outlook.com>
2023-11-05 16:32:22 -0300

    adicionado o método que lista os alunos cadastrados.

--- cadastro_de_aluno/src/main/java/com/outlook/rafael_taparica/helper/Helper.java
+++ cadastro_de_aluno/src/main/java/com/outlook/rafael_taparica/helper/Helper.java
@@ -1,5 +1,7 @@
 package com.outlook.rafael_taparica.helper;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import com.outlook.rafael_taparica.entity.Aluno;
@@ -12,11 +14,10 @@ public class Helper {
 		this.em = em;
 	}
 
-	/* INSERT
-	 * método para adicionar um novo aluno.
+	/*
+	 * INSERT método para adicionar um novo aluno.
 	 */
-	public String adicionarAluno(String nome, String cpf, String email, String telefone,
-			String dataNascimento) {
+	public String adicionarAluno(String nome, String cpf, String email, String telefone, String dataNascimento) {
 
 		Aluno aluno = new Aluno();
 		aluno.setNome(nome);
@@ -33,8 +34,8 @@ public class Helper {
 
 	}
 
-	/*DELETE
-	 * método para deletar um aluno pelo seu id.
+	/*
+	 * DELETE método para deletar um aluno pelo seu id.
 	 */
 	public String deletarAluno(int idAluno) {
 
@@ -47,35 +48,51 @@ public class Helper {
 
 		return "aluno deletado";
 	}
-	
-	
-	/* MODIFY
-	 * método para atualizar e-mail do aluno.
+
+	/*
+	 * MODIFY método para atualizar e-mail do aluno.
 	 */
-	public String atualizarEmail (int idAluno, String email) {
-		
+	public String atualizarEmail(int idAluno, String email) {
+
 		Aluno aluno = em.find(Aluno.class, idAluno);
 		aluno.setEmail(email);
-		
+
 		em.getTransaction().begin();
 		em.merge(aluno);
 		em.getTransaction().commit();
-		
+
 		return "idade atualizada";
 	}
 
-	/* MODIFY
-	 * método para atualizar o telefone do aluno.
+	/*
+	 * MODIFY método para atualizar o telefone do aluno.
 	 */
-	public String atualizarTelefone (int idAluno, String telefone) {
-		
+	public String atualizarTelefone(int idAluno, String telefone) {
+
 		Aluno aluno = em.find(Aluno.class, idAluno);
 		aluno.setTelefone(telefone);
-		
+
 		em.getTransaction().begin();
 		em.merge(aluno);
 		em.getTransaction().commit();
-		
+
 		return "contato atualizada";
+	}
+
+	/*
+	 * Método para listar todos os alunos, sem paginação.
+	 */
+
+	public void listaDeAlunos() {
+
+		List<Aluno> listaCompleta = em.createQuery("SELECT a FROM Aluno a", Aluno.class).getResultList();
+
+		for (Aluno listaDeAlunos : listaCompleta) {
+
+			System.out.println("Id: " + listaDeAlunos.getIdAluno() + " Nome: " + listaDeAlunos.getNome() + " cpf: "
+					+ listaDeAlunos.getCpf() + " Telefone: " + listaDeAlunos.getTelefone() + " Data de nascimento: "
+					+ listaDeAlunos.getDataNascimento());
+		}
+
 	}
 }
