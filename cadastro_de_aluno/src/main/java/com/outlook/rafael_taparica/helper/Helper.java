d6b67df3b0782762418cd0ae318f6b7477c8ba33
RafaelTaparica <rafael_taparica@outlook.com>
2023-11-05 13:35:36 -0300

    adição dos métodos de deletar aluno, atualizar e-mail e telefone.

--- cadastro_de_aluno/src/main/java/com/outlook/rafael_taparica/helper/Helper.java
+++ cadastro_de_aluno/src/main/java/com/outlook/rafael_taparica/helper/Helper.java
@@ -12,12 +12,14 @@ public class Helper {
 		this.em = em;
 	}
 
-	public String adicionarAluno(String nome, int idade, String cpf, String email, String telefone,
+	/* INSERT
+	 * método para adicionar um novo aluno.
+	 */
+	public String adicionarAluno(String nome, String cpf, String email, String telefone,
 			String dataNascimento) {
 
 		Aluno aluno = new Aluno();
 		aluno.setNome(nome);
-		aluno.setIdade(idade);
 		aluno.setCpf(cpf);
 		aluno.setEmail(email);
 		aluno.setTelefone(telefone);
@@ -31,4 +33,49 @@ public class Helper {
 
 	}
 
+	/*DELETE
+	 * método para deletar um aluno pelo seu id.
+	 */
+	public String deletarAluno(int idAluno) {
+
+		Aluno aluno = em.find(Aluno.class, idAluno);
+		aluno.getIdAluno();
+
+		em.getTransaction().begin();
+		em.remove(aluno);
+		em.getTransaction().commit();
+
+		return "aluno deletado";
+	}
+	
+	
+	/* MODIFY
+	 * método para atualizar e-mail do aluno.
+	 */
+	public String atualizarEmail (int idAluno, String email) {
+		
+		Aluno aluno = em.find(Aluno.class, idAluno);
+		aluno.setEmail(email);
+		
+		em.getTransaction().begin();
+		em.merge(aluno);
+		em.getTransaction().commit();
+		
+		return "idade atualizada";
+	}
+
+	/* MODIFY
+	 * método para atualizar o telefone do aluno.
+	 */
+	public String atualizarTelefone (int idAluno, String telefone) {
+		
+		Aluno aluno = em.find(Aluno.class, idAluno);
+		aluno.setTelefone(telefone);
+		
+		em.getTransaction().begin();
+		em.merge(aluno);
+		em.getTransaction().commit();
+		
+		return "contato atualizada";
+	}
 }
